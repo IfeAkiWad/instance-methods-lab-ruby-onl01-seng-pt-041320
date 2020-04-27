da502056e0c3e7a5bca36c27de299407563f584b
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 21:47:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog #defining a class as Dog. Within this class there is a Instance method. this is where we will "teach" the dogs in this class to something
-def bark #this is an instance method
-  puts "Woof!"
+def bark #this is an instance method. it is called bark.
+  puts "Woof!" #here are inputting what the dogs will be able to do.
 end
 
 def sit
-  puts "The Dog is sitting"
+  puts "The Dog is sitting" #here are inputting what the dogs will be able to do.
 end
 end
