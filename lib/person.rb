104e5ef206625af795625f509208c325db8106bc
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 21:47:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   def talk
     puts "Hello World!" #This onstance method we built inside this Person class, gives our person the ability to do something (talk)
   end
-  def walk
+  def walk #this instance now can also walk because of this instance method.
     puts "The Person is walking"
   end
 end
