b85612990018315beec7a5ab22bc3c20864c116d
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 21:42:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,4 @@
-class Dog
+class Dog #defining a class as Dog. Within this class there is a Instance method. this is where we will "teach" the dogs in this class to something
 def bark
   puts "Woof!"
 end
